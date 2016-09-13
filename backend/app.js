f452b63ab59754d0b0f4775a14ea1acae13fde21
Felipe Diniz <diniz.felipe@eb.mil.br>
2016-09-12 21:19:38 -0300

    Mudanca inicial do backend

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,15 @@
-(function() {
-  "use strict";
+(function () {
+  'use strict';
 
   var express = require('express');
   var path = require('path');
-  var favicon = require('serve-favicon');
+
   // log requests - just for dev
   var logger = require('morgan');
 
   var routes = require('./routes/index');
 
   var app = express();
-
-  app.use(favicon(__dirname + '/public/favicon.ico'));
 
   // view engine setup
   app.set('views', path.join(__dirname, 'views'));
@@ -22,52 +20,50 @@
   app.use(express.static(path.join(__dirname, 'public')));
 
   //Lower case query parameters
-  app.use(function(req, res, next) {
-    for (var key in req.query)
-    {
+  app.use(function (req, res, next) {
+    for (var key in req.query) {
       req.query[key.toLowerCase()] = req.query[key];
     }
+
     next();
   });
 
   app.use('/', routes);
 
   // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
+  app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
   });
 
   //clear cache
-
-  app.use(function(req, res, next){
-
-    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
-    res.header("Pragma", "no-cache");
-    res.header("Expires",0);
+  app.use(function (req, res, next) {
+    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.header('Pragma', 'no-cache');
+    res.header('Expires', 0);
     next();
-});
+  });
 
-app.disable('etag')
+  app.disable('etag');
 
   // development error handler
   // will print stacktrace
   if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
       res.status(err.status || 500);
       res.render('error', {
         message: err.message,
-        error: err
+        error: err,
       });
     });
   }
 
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: {}
+      error: {},
     });
   });
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,61 +1,47 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
-var cors = require('cors');
+(function () {
+  'use strict';
 
-// create application/json parser
-var jsonParser = bodyParser.json()
+  var express = require('express');
+  var router = express.Router();
+  var bodyParser = require('body-parser');
+  var cors = require('cors');
 
-//Javascript com operações de banco
-var operations = require('../src/operations');
+  // create application/json parser
+  var jsonParser = bodyParser.json();
 
+  //Javascript com operações de banco
+  var operations = require('../src/operations');
 
-//CORS middleware
-router.use(cors({
-  exposedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Link, Location"
-}));
-// router.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+  //CORS middleware
+  router.use(cors({
+    exposedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Link, Location',
+  }));
 
-var availableLayers = ['usuarios', 'funcoes', 'secoes', 'projetos', 'subprojetos',
-'fases', 'subfases', 'tarefas', 'atividades', 'atividadesespeciais', 'tipoatividadesespecial', 'molduras']
+  // router.use(function(req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //   next();
+  // });
 
-var layersVerify = function (req, res, next) {
-	if (availableLayers.indexOf(req.params.layer) != -1) {
-		next();
-	} else {
-		res.status(404).end();
-	}
-};
+  router.get('/equipamentos', function (req, res, next) {
+    operations.getEquipamentos(req, res, next);
+  });
 
+  router.get('/cautelas', function (req, res, next) {
+    operations.getCautelas(req, res, next);
+  });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  router.get('/cautelas/:id', function (req, res, next) {
+    operations.getCautelasByEquipId(req, res, req.params.id, next);
+  });
 
-router.post('/login', jsonParser, function(req, res, next) {
-  operations.login(req, res);
-});
+  router.get('/manutencoes', function (req, res, next) {
+    operations.getManutencoes(req, res, next);
+  });
 
+  router.get('/manutencoes/:id', function (req, res, next) {
+    operations.getManutencoesByEquipId(req, res, req.params.id, next);
+  });
 
-router.get('/data/:layer', layersVerify, function(req, res, next) {
-  operations.get[req.params.layer](req, res);
-});
-
-router.post('/data/:layer', layersVerify, jsonParser, function(req, res, next) {
-  operations.post[req.params.layer](req, res);
-});
-
-router.delete('/data/:layer/:id', layersVerify, function(req, res, next) {
-  operations.delete[req.params.layer](res, req.params.id);
-});
-
-router.put('/data/:layer/:id', layersVerify, jsonParser, function(req, res, next) {
-  operations.put[req.params.layer](req, res, req.params.id);
-});
-
-module.exports = router;
+  module.exports = router;
+})();
