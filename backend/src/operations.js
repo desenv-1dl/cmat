98647d921fc92ef4acceaeddd5cc672a3d2efcd7
Felipe Diniz <diniz@1DL.EB.MIL.BR>
2016-09-20 16:37:55 -0300

    Ajustando quanto mudança de modelo

--- backend/src/operations.js
+++ backend/src/operations.js
@@ -17,7 +17,8 @@
         t.any('SELECT * FROM equipamento'),
         t.any('SELECT ce.*, c.* FROM cautela AS c INNER JOIN cautela_equipamento AS ce ' +
         'ON c.id = ce.cautela_id'),
-        t.any('SELECT * FROM manutencao'),
+        t.any('SELECT me.*, m.* FROM manutencao AS m INNER JOIN manutencao_equipamento AS me ' +
+        'ON m.id = me.manutencao_id'),
       ]);
     }).then(function (data) {
       data[0].forEach(function (e) {
@@ -49,8 +50,8 @@
   function getCautelas(req, res, next) {
     db.task(function (t) {
       return t.batch([
-        db.any('SELECT * FROM cautela'),
-        db.any('SELECT ce.*, e.* FROM equipamento AS e INNER JOIN cautela_equipamento AS ce ' +
+        t.any('SELECT * FROM cautela'),
+        t.any('SELECT ce.*, e.* FROM equipamento AS e INNER JOIN cautela_equipamento AS ce ' +
         'ON e.id = ce.equipamento_id'),
       ]);
     }).then(function (data) {
@@ -75,42 +76,42 @@
   }
 
   function getManutencoes(req, res, next) {
-    //resolução de conflito de nomes de atributos para pegar id de manutencao
-    db.any('SELECT e.*, m.* FROM manutencao AS m INNER JOIN equipamento AS e ' +
-          ' ON m.equipamento_id = e.id')
-      .then(function (data) {
-        data.forEach(function (m) {
-          m.data_inicio = m.data_inicio.toISOString().split('T')[0];
-          m.data_fim = m.data_fim ? m.data_fim.toISOString().split('T')[0] : null;
-          m.equipamento = {};
-          m.equipamento.id = m.equipamento_id;
-          m.equipamento.nome = m.nome;
-          m.equipamento.fabricante = m.fabricante;
-          m.equipamento.tipo = m.tipo;
-          m.equipamento.carga = m.carga;
-          m.equipamento.situacao = m.situacao;
-          m.equipamento.foto = m.foto;
-
-          delete m.equipamento_id;
-          delete m.nome;
-          delete m.fabricante;
-          delete m.tipo;
-          delete m.carga;
-          delete m.disponivel;
-          delete m.foto;
-
+    db.task(function (t) {
+      return t.batch([
+        t.any('SELECT * FROM manutencao'),
+        t.any('SELECT me.*, e.* FROM equipamento AS e INNER JOIN manutencao_equipamento AS me ' +
+        'ON e.id = me.equipamento_id'),
+      ]);
+    }).then(function (data) {
+      data[0].forEach(function (c) {
+        c.equipamentos = [];
+        c.data_inicio = c.data_inicio.toISOString().split('T')[0];
+        c.data_fim = c.data_fim ? c.data_fim.toISOString().split('T')[0] : null;
+        data[1].forEach(function (e) {
+          if (e.manutencao_id === c.id) {
+            delete e.manutencao_id;
+            delete e.equipamento_id;
+            c.equipamentos.push(e);
+          }
         });
-
-        res.status(200).json(data);
-      })
-      .catch(function (err) {
-        return next(err);
       });
+
+      res.status(200).json(data[0]);
+    })
+    .catch(function (err) {
+      return next(err);
+    });
   }
 
+
+
   function postEquipamentos(req, res, data, next) {
-    db.none('INSERT INTO equipamento(nome, fabricante, tipo, carga, situacao)' +
-        'values(${nome}, ${fabricante}, ${tipo}, ${carga}, ${situacao})',
+    if (!data.foto) {
+      data.foto = null;
+    }
+
+    db.none('INSERT INTO equipamento(nome, fabricante, tipo, carga, situacao, foto)' +
+        'values(${nome}, ${fabricante}, ${tipo}, ${carga}, ${situacao}, ${foto})',
       data)
       .then(function () {
         res.status(200)
@@ -128,14 +129,25 @@
     if (!data.data_fim) {
       data.data_fim = null;
     }
-
     if (!data.valor) {
       data.valor = null;
     }
+    db.tx(function (t) {
+        return t.one('INSERT INTO manutencao(data_inicio, data_fim, descricao, empresa, ' +
+        'valor) values(${data_inicio}, ${data_fim}, ${descricao}, ' + 
+        '${empresa}, ${valor}) returning id', data)
+        .then(function (resp) {
+          var queries = [];
+          data.equipamentos.forEach(function (e) {
+            queries.push(
+              t.none('INSERT INTO manutencao_equipamento(equipamento_id, manutencao_id)' +
+              'values($1, $2)', [e, resp.id])
+            );
+          });
 
-    db.none('INSERT INTO manutencao(data_inicio, data_fim, descricao, empresa,' +
-        'valor, equipamento_id) values(${data_inicio}, ${data_fim}, ${descricao},' +
-        '${empresa}, ${valor}, ${equipamento_id})', data)
+          return t.batch(queries);
+        });
+      })
       .then(function () {
         res.status(200)
           .json({
@@ -152,19 +164,21 @@
     if (!data.data_fim) {
       data.data_fim = null;
     }
-
+    if (!data.observacao) {
+      data.observacao = null;
+    }
     //gera numero da cautela automaticamente. Padrão ANO - nº
 
     var anoAtual = new Date().getFullYear();
     db.tx(function (t) {
-        return db.one('SELECT numero FROM cautela WHERE numero LIKE \'$1-%\' ' +
+        return t.one('SELECT numero FROM cautela WHERE numero LIKE \'$1-%\' ' +
               ' ORDER BY numero DESC LIMIT 1', anoAtual)
           .then(function (result) {
             data.numero = anoAtual + '-' + (parseInt(result.numero.split('-')[1]) + 1).toString();
             console.log(data.numero)
 
-            return t.one('INSERT INTO cautela(numero, operador, missao, data_inicio, data_fim, situacao)' +
-            'values(${numero}, ${operador}, ${missao}, ${data_inicio}, ${data_fim}, ${situacao}) returning id',
+            return t.one('INSERT INTO cautela(numero, operador, missao, data_inicio, data_fim, situacao, observacao)' +
+            'values(${numero}, ${operador}, ${missao}, ${data_inicio}, ${data_fim}, ${situacao}, observacao) returning id',
             data).then(function (resp) {
               var queries = [];
               data.equipamentos.forEach(function (e) {
@@ -194,45 +208,22 @@
     if (!data.data_fim) {
       data.data_fim = null;
     }
-
     if (!data.valor) {
       data.valor = null;
     }
 
-    db.none('UPDATE manutencao SET data_inicio=$1, data_fim=$2, descricao=$3, ' +
-            'empresa=$4, valor=$5, equipamento_id=$6 where id=$7',
-            [data.data_inicio, data.data_fim, data.descricao, data.empresa,
-            data.valor, data.equipamento_id, id,
-      ])
-      .then(function () {
-        res.status(200)
-        .json({
-          status: 'success',
-          message: 'Manutenção atualizada',
-        });
-      })
-      .catch(function (err) {
-        return next(err);
-      });
-  }
-
-  function putCautelas(req, res, id, data, next) {
-    if (!data.data_fim) {
-      data.data_fim = null;
-    }
-
     db.tx(function (t) {
-        return t.none('UPDATE cautela SET numero=$1, operador=$2, data_inicio=$3, ' +
-        'data_fim=$4 where id=$5',
+        return t.none('UPDATE manutencao SET empresa=$1, descricao=$2, data_inicio=$3, ' +
+        'data_fim=$4, valor=$5 where id=$6',
         [
-          data.numero, data.operador, data.data_inicio, data.data_fim, id,
+          data.empresa, data.descricao, data.data_inicio, data.data_fim, data.valor, id,
         ]).then(function () {
-          return t.none('DELETE from cautela_equipamento where cautela_id=$1', id)
+          return t.none('DELETE from manutencao_equipamento where manutencao_id=$1', id)
             .then(function () {
               var queries = [];
               data.equipamentos.forEach(function (e) {
                 queries.push(
-                  t.none('INSERT INTO cautela_equipamento(equipamento_id, cautela_id)' +
+                  t.none('INSERT INTO manutencao_equipamento(equipamento_id, manutencao_id)' +
                   'values($1, $2)', [e, id])
                 );
               });
@@ -253,8 +244,71 @@
       });
   }
 
+  function putCautelas(req, res, id, data, next) {
+    if (!data.data_fim) {
+      data.data_fim = null;
+    }
+    if (!data.observacao) {
+      data.observacao = null;
+    }
+
+    db.tx(function (t) {
+        return t.none('UPDATE cautela SET missao=$1, operador=$2, data_inicio=$3, ' +
+        'data_fim=$4, situacao=$5, observacao=$6 where id=$7',
+        [
+          data.missao, data.operador, data.data_inicio, data.data_fim, data.situacao, data.observacao, id,
+        ]).then(function () {
+          return t.none('DELETE from cautela_equipamento where cautela_id=$1', id)
+            .then(function () {
+              var queries = [];
+              data.equipamentos.forEach(function (e) {
+                queries.push(
+                  t.none('INSERT INTO cautela_equipamento(equipamento_id, cautela_id)' +
+                  'values($1, $2)', [e, id])
+                );
+              });
+
+              return t.batch(queries);
+            });
+        });
+      })
+      .then(function () {
+        res.status(200)
+        .json({
+          status: 'success',
+          message: 'Cautela atualizada',
+        });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  }
+
+  function putEquipamentos(req, res, id, data, next) {
+    if (!data.foto) {
+      data.foto = null;
+    }
+  
+    db.none('UPDATE equipamento SET nome=$1, fabricante=$2, tipo=$3, ' +
+    'carga=$4, situacao=$5, foto=$6 where id=$7',
+    [
+      data.nome, data.fabricante, data.tipo, data.carga, data.situacao, data.foto, id,
+    ])
+    .then(function () {
+      res.status(200)
+      .json({
+        status: 'success',
+        message: 'Equipamento atualizado',
+      });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  }
+
   module.exports.getEquipamentos = getEquipamentos;
   module.exports.postEquipamentos = postEquipamentos;
+   module.exports.putEquipamentos = putEquipamentos;
   module.exports.getCautelas = getCautelas;
   module.exports.postCautelas = postCautelas;
   module.exports.putCautelas = putCautelas;
